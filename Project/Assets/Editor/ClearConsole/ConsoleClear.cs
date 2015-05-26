35f0712a995f1a107368288128c13cac610269c1
Kipras <kipras@live.com>
2015-05-26 14:34:10 +0300

    CTRL+SHIFT+C to clear console

---
+++ Project/Assets/Editor/ClearConsole/ConsoleClear.cs
@@ -0,0 +1,16 @@
+﻿// C# editor script
+using UnityEngine;
+using UnityEditor;
+
+static class ConsoleClear
+{
+    [MenuItem("Tools/Clear Console %#c")] // CMD + SHIFT + C
+    static void ClearConsole()
+    {
+        // This simply does "LogEntries.Clear()" the long way:
+        var logEntries = System.Type.GetType("UnityEditorInternal.LogEntries,UnityEditor.dll");
+        var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+        clearMethod.Invoke(null, null);
+    }
+
+}
