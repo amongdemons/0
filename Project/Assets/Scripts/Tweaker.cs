41757a700f1b032a8405146d6f1b6a2c527189ec
Kipras <kipras@live.com>
2015-05-26 14:16:51 +0300

    Vanilla+

---
+++ Project/Assets/Scripts/Tweaker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Tweaker : MonoBehaviour
+{
+    public int frameRateLimit = 120;
+    public bool limitFrameRate;
+    public bool hideCursor;
+    public bool lockCursor;
+
+    void Awake()
+    {
+        if (limitFrameRate == true)
+        {
+            Application.targetFrameRate = frameRateLimit;
+            Debug.Log("Frame Rate Limit is set to:  " + frameRateLimit);
+        }
+        else { Debug.Log("Frame Rate Limit is disabled."); }
+
+        if (hideCursor == true)
+        {
+            Cursor.visible = false;
+        }
+
+        if (lockCursor == true)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
